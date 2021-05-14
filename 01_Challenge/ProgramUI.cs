5730b609c2af9b88c10ffefa1738d8423551e1a1
James Campbell <jaymznis@gmail.com>
2021-05-14 14:13:34 -0400

    menu repo made

--- 01_Challenge/ProgramUI.cs
+++ 01_Challenge/ProgramUI.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace _01_Challenge
 {
-    class Class1
+    class ProgramUI
     {
     }
 }
--- 01_ChallengeRepository/MenuRepository.cs
+++ 01_ChallengeRepository/MenuRepository.cs
@@ -6,7 +6,67 @@ using System.Threading.Tasks;
 
 namespace _01_ChallengeRepository
 {
-    public class Class1
+    public class MenuRepository
     {
+        private readonly List<MenuItem> _menuItems = new List<MenuItem>();
+
+        public bool AddMenuItem(MenuItem newItem)
+        {
+            int existingItems = _menuItems.Count;
+
+           _menuItems.Add(newItem);
+            bool wasAdded = (_menuItems.Count > existingItems) ? true : false;
+            return wasAdded;
+        }
+        public List<MenuItem> GetMenu()
+        {
+            return _menuItems;
+        }
+
+        public MenuItem GetMenuItemByNumber(int menuNumber)
+        {
+            foreach (MenuItem item in _menuItems)
+            {
+                if(item.MealNumber == menuNumber && item.GetType() == typeof(MenuItem))
+                {
+                    return (MenuItem)item;
+                }
+            }
+            return null;
+        }
+        public bool UpdateMenuItemByNumber(int menuNum, MenuItem newMenuItem)
+        {
+            MenuItem oldMenuItem = GetMenuItemByNumber(menuNum);
+            {
+                if(oldMenuItem != null)
+                {
+                    oldMenuItem.MealNumber = newMenuItem.MealNumber;
+                    oldMenuItem.MealName = newMenuItem.MealName;
+                    oldMenuItem.MealDescription = newMenuItem.MealDescription;
+                    oldMenuItem.Ingredients = newMenuItem.Ingredients;
+                    oldMenuItem.Price = newMenuItem.Price;
+
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+        }
+
+        public bool DeleteMenuByNumber(int menuNum)
+        {
+            MenuItem itemToDelete = GetMenuItemByNumber(menuNum);
+            if(itemToDelete == null)
+            {
+                return false;
+            }
+            else
+            {
+                _menuItems.Remove(itemToDelete);
+                return true;
+            }
+        }
     }
 }
