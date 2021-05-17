7820a1df9e23759ea71b5a8a0c9e338ded0c7ae9
James Campbell <jaymznis@gmail.com>
2021-05-17 16:58:48 -0400

    adding new claims works

--- 02_Challenge/ClaimsProgramUI.cs
+++ 02_Challenge/ClaimsProgramUI.cs
@@ -87,34 +87,103 @@ namespace _02_Challenge
             Console.Clear();
             Claims newClaim = new Claims();
 
-            Console.WriteLine("\n\t Enter Claim ID:");
+            Console.Write("\n\t Enter Claim ID#: ");
             newClaim.ClaimID = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("\n\t What is the Claim Type?\n" +
+            Console.Clear();
+
+            Console.Write("\n\t What is the Claim Type?\n" +
                 "\t1. Car\n" +
                 "\t2. Home\n" +
-                "\t3. Theft\n");
+                "\t3. Theft\n" +
+                "\n\t");
             string input = Console.ReadLine();
-            switch (input.ToLower)
+            switch (input.ToLower())
             {
                 case "1":
                 case "one":
                 case "car":
-                    newClaim.Type(1);
+                    newClaim.Type = ClaimType.Car;
                     break;
+                case "2":
+                case "two":
+                case "home":
+                    newClaim.Type = ClaimType.Home;
+                    break;
+                case "3":
+                case "three":
+                case "theft":
+                    newClaim.Type = ClaimType.Theft;
+                    break;
+                default:
+                    Console.WriteLine("\t\n That is not an available option");
+                    Console.ReadKey();
+                    NewClaim();
+                    break;
+            }
+            Console.Clear();
 
+            Console.Write("\n\t Enter Claim Description:\n" +
+                "\n\t");
+            newClaim.Description = Console.ReadLine();
+            Console.Clear();
 
+            Console.Write("\n\t Eneter Cost of Damage: \n" +
+                "\n\t $");
+            newClaim.Amount = Convert.ToDouble(Console.ReadLine());
+            Console.Clear();
 
+            Console.Write("\n\t Enter Date of Accident:\n" +
+                "\tExample: mm dd yyyy or January 1 2021\n" +
+                "\n\t");
+            DateTime accidnetDate;
+            if (DateTime.TryParse(Console.ReadLine(), out accidnetDate))
+            {
+                newClaim.DateOfAccident = accidnetDate;
+            }
+            else
+            {
+                Console.WriteLine("\n\t That format can not be recognized");
+                Console.ReadKey();
+                NewClaim();
             }
 
-            private void SeedContent()
+            Console.Write("\n\t Enter Date of Claim:\n" +
+                "\tExample: mm dd yyyy or January 1 2021\n" +
+                "\n\t"); 
+            DateTime claimDate;
+            if (DateTime.TryParse(Console.ReadLine(), out claimDate))
             {
-                Claims c1 = new Claims(1, ClaimType.Car, "Car accident on 465", 400, new DateTime(2018, 04, 25), new DateTime(2018, 04, 27));
-                Claims c2 = new Claims(2, ClaimType.Home, "House fire in kitchen.", 4000, new DateTime(2018, 04, 11), new DateTime(2018, 04, 12));
-                Claims c3 = new Claims(3, ClaimType.Theft, "Stolen pancakes.", 4, new DateTime(2018, 04, 27), new DateTime(2018, 06, 02));
+                newClaim.DateOfClaim = claimDate;
+            }
+            else
+            {
+                Console.WriteLine("\n\t That format can not be recognized");
+                Console.ReadKey();
+                NewClaim();
+            }
+            Console.Clear();
 
-                _repo.AddClaim(c1);
-                _repo.AddClaim(c2);
-                _repo.AddClaim(c3);
+
+            bool wasAdded = _repo.AddClaim(newClaim);
+            if (wasAdded)
+            {
+                Console.WriteLine("\n\t Claim has been added");
+            }
+            else
+            {
+                Console.WriteLine("\n\t Claim could not be added");
             }
         }
+        private void SeedContent()
+        {
+            Claims c1 = new Claims(1, ClaimType.Car, "Car accident on 465", 400, new DateTime(2018, 04, 25), new DateTime(2018, 04, 27));
+            Claims c2 = new Claims(2, ClaimType.Home, "House fire in kitchen.", 4000, new DateTime(2018, 04, 11), new DateTime(2018, 04, 12));
+            Claims c3 = new Claims(3, ClaimType.Theft, "Stolen pancakes.", 4, new DateTime(2018, 04, 27), new DateTime(2018, 06, 02));
+
+            _repo.AddClaim(c1);
+            _repo.AddClaim(c2);
+            _repo.AddClaim(c3);
+        }
     }
+
+}
