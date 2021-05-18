5593e4f83a263fc4acdd545ec31a6562bc604f7c
James Campbell <jaymznis@gmail.com>
2021-05-18 11:08:51 -0400

    challenge 2 test methods done

---
+++ 02_ChallengeTestMethod/ClaimsCRUDTests.cs
@@ -0,0 +1,70 @@
+﻿using _02_ChallengeRepository;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+
+namespace _02_ChallengeTestMethod
+{
+    [TestClass]
+    public class ClaimsCRUDTests
+    {
+        [TestMethod]
+        public void CreatClaimTestShouldWork()
+        {
+            Claims claim = new Claims();
+            ClaimsRepo repo = new ClaimsRepo();
+
+            bool add = repo.AddClaim(claim);
+
+            Assert.IsTrue(add);
+        }
+
+        public void ReadClaims()
+        {
+            Claims claim = new Claims();
+            ClaimsRepo repo = new ClaimsRepo();
+            repo.AddClaim(claim);
+
+            Queue<Claims> queue = repo.GetClaims();
+            bool newQueue = queue.Contains(claim);
+            Assert.IsTrue(newQueue);
+        }
+
+        private ClaimsRepo _repo;
+        private Claims _claim;
+
+        [TestInitialize]
+        public void SeedTestMethod()
+        {
+            _repo = new ClaimsRepo();
+            _claim = new Claims(1, ClaimType.Car, "Car accident on 465", 400, new DateTime(2018, 04, 25), new DateTime(2018, 04, 27));
+
+
+            _repo.AddClaim(_claim);
+        }
+        [TestMethod]
+        public void UpdateClaims()
+        {
+            Assert.AreEqual(_claim.ClaimID, 1);
+            Assert.AreEqual(_claim.Amount, 400);
+
+            _repo.UpdateClaim(1, new Claims(2, ClaimType.Home, "House fire in kitchen.", 4000, new DateTime(2018, 04, 11), new DateTime(2018, 04, 12)));
+
+            Assert.AreEqual(_claim.ClaimID, 2);
+            Assert.AreEqual(_claim.Amount, 4000);
+
+        }
+        public void DeleteClaim()
+        {
+            bool deleted = _repo.DeleteClaimByID(2);
+
+            Assert.IsTrue(deleted);
+
+            _repo.AddClaim(_claim);
+
+            bool deQueued = _repo.DequeueClaims();
+            Assert.IsTrue(deQueued);
+        }
+
+    }
+}
---
+++ 02_ChallengeTestMethod/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("02_ChallengeTestMethod")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("02_ChallengeTestMethod")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("3661d9a9-b76b-4a99-b660-f0098f37da7b")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
