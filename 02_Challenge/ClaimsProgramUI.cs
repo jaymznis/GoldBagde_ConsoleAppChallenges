6b2f47308c6d0bc54b3f0392d292196a4bcc2fe1
James Campbell <jaymznis@gmail.com>
2021-05-18 10:38:51 -0400

    added empty queue method to challenge 2

--- 02_Challenge/ClaimsProgramUI.cs
+++ 02_Challenge/ClaimsProgramUI.cs
@@ -20,11 +20,12 @@ namespace _02_Challenge
         {
             //make it username and password enabled?
             Console.WriteLine("\n\tKomodo Claims Department Access");
+            Console.WriteLine("\n\tPress any Key to Continue");
+
 
             bool keepRunning = true;
             while (keepRunning)
             {
-                Console.WriteLine("\n\tPress any Key to Continue");
                 Console.ReadKey();
                 Console.Clear();
                 Console.WriteLine("\n\tPlease select an option: \n" +
@@ -74,20 +75,27 @@ namespace _02_Challenge
         private void SeeAllClaims()
         {
             Console.Clear();
-            Queue<Claims> allClaims = _repo.GetClaims();
-            //Console.WriteLine($"\t Claim ID \t\t Type \t\t Description \t\t Amount \t\t Date Of Accident \t\t Date Of Claim \t\t Claim is Valid");
-            //Console.WriteLine(".......................................................................................................................");
-            foreach (Claims claim in allClaims)
+            if (_repo.ClaimsCount() > 0)
             {
-                Console.WriteLine($"\n\t ClaimID: #{claim.ClaimID}\n" +
-               $"\t Type: \t\t\t{claim.Type}\n" +
-               $"\t Description: \t\t{claim.Description}\n" +
-               $"\t Amount: \t\t${claim.Amount}\n" +
-               $"\t Date of Accident: \t{claim.DateOfAccident}\n" +
-               $"\t Date of Claim: \t{claim.DateOfClaim}\n" +
-               $"\t IsValid: \t\t{claim.IsValid}\n" +
-               $"..................................................................................");
-                //Console.WriteLine($"\t {claim.ClaimID} \t\t {claim.Type} \t\t {claim.Description} \t\t {claim.Amount} \t\t {claim.DateOfAccident} \t\t {claim.DateOfClaim} \t\t {claim.IsValid}\n");
+                Queue<Claims> allClaims = _repo.GetClaims();
+                //Console.WriteLine($"\t Claim ID \t\t Type \t\t Description \t\t Amount \t\t Date Of Accident \t\t Date Of Claim \t\t Claim is Valid");
+                //Console.WriteLine(".......................................................................................................................");
+                foreach (Claims claim in allClaims)
+                {
+                    Console.WriteLine($"\n\t ClaimID: #{claim.ClaimID}\n" +
+                   $"\t Type: \t\t\t{claim.Type}\n" +
+                   $"\t Description: \t\t{claim.Description}\n" +
+                   $"\t Amount: \t\t${claim.Amount}\n" +
+                   $"\t Date of Accident: \t{claim.DateOfAccident}\n" +
+                   $"\t Date of Claim: \t{claim.DateOfClaim}\n" +
+                   $"\t IsValid: \t\t{claim.IsValid}\n" +
+                   $"..................................................................................");
+                    //Console.WriteLine($"\t {claim.ClaimID} \t\t {claim.Type} \t\t {claim.Description} \t\t {claim.Amount} \t\t {claim.DateOfAccident}} \t\t {claim.DateOfClaim} \t\t {claim.IsValid}\n");
+                }
+            }
+            else
+            {
+                Console.WriteLine("\n\t Queue is empty ");
             }
         }
 
@@ -105,18 +113,19 @@ namespace _02_Challenge
         private void NextClaim()
         {
             Console.Clear();
-            if (_repo.ClaimsCount() > 0) { 
-            Claims nextClaim = _repo.ClaimsPeek();
-            Console.WriteLine($"\n\t ClaimID: #{nextClaim.ClaimID}\n" +
-                $"\t Type: \t\t\t{nextClaim.Type}\n" +
-                $"\t Description: \t\t{nextClaim.Description}\n" +
-                $"\t Amount: \t\t${nextClaim.Amount}\n" +
-                $"\t Date of Accident: \t{nextClaim.DateOfAccident}\n" +
-                $"\t Date of Claim: \t{nextClaim.DateOfClaim}\n" +
-                $"\t IsValid: \t\t{nextClaim.IsValid}");
+            if (_repo.ClaimsCount() > 0)
+            {
+                Claims nextClaim = _repo.ClaimsPeek();
+                Console.WriteLine($"\n\t ClaimID: #{nextClaim.ClaimID}\n" +
+                    $"\t Type: \t\t\t{nextClaim.Type}\n" +
+                    $"\t Description: \t\t{nextClaim.Description}\n" +
+                    $"\t Amount: \t\t${nextClaim.Amount}\n" +
+                    $"\t Date of Accident: \t{nextClaim.DateOfAccident}\n" +
+                    $"\t Date of Claim: \t{nextClaim.DateOfClaim}\n" +
+                    $"\t IsValid: \t\t{nextClaim.IsValid}");
 
-            Console.Write("\n\t Would you like to take care of this claim? y/n");
-            string input = Console.ReadLine();
+                Console.Write("\n\t Would you like to take care of this claim? y/n ");
+                string input = Console.ReadLine();
                 switch (input.ToLower())
                 {
                     case "y":
@@ -142,7 +151,7 @@ namespace _02_Challenge
             }
             else
             {
-                Console.WriteLine("\n\t There are no more claims");
+                Console.WriteLine("\n\t There are no more claims to take care of. \n");
             }
         }
 
@@ -240,69 +249,84 @@ namespace _02_Challenge
         }
 
         private void LookUpClaim()
-        {//add method to show claim number and date of claim
+        {
             Console.Clear();
-            SeeIDandDate();
-            Console.Write("\n\t Enter Claim ID#:");
-            Claims claim = _repo.GetClaimsByID(Convert.ToInt32(Console.ReadLine()));
-
-            if (claim != null)
+            if (_repo.ClaimsCount() > 0)
             {
-                Console.WriteLine($"\n\t ClaimID: #{claim.ClaimID}\n" +
-              $"\t Type: \t\t\t{claim.Type}\n" +
-              $"\t Description: \t\t{claim.Description}\n" +
-              $"\t Amount: \t\t${claim.Amount}\n" +
-              $"\t Date of Accident: \t{claim.DateOfAccident}\n" +
-              $"\t Date of Claim: \t{claim.DateOfClaim}\n" +
-              $"\t IsValid: \t\t{claim.IsValid}\n" +
-              $"..................................................................................");
-                // Console.WriteLine($"\t {claim.ClaimID} \t\t {claim.Type} \t\t {claim.Description} \t\t {claim.Amount} \t\t {claim.DateOfAccident} \t\t {claim.DateOfClaim} \t\t {claim.IsValid}\n");
+                SeeIDandDate();
+                Console.Write("\n\t Enter Claim ID#:");
+                Claims claim = _repo.GetClaimsByID(Convert.ToInt32(Console.ReadLine()));
+
+                if (claim != null)
+                {
+                    Console.WriteLine($"\n\t ClaimID: #{claim.ClaimID}\n" +
+                  $"\t Type: \t\t\t{claim.Type}\n" +
+                  $"\t Description: \t\t{claim.Description}\n" +
+                  $"\t Amount: \t\t${claim.Amount}\n" +
+                  $"\t Date of Accident: \t{claim.DateOfAccident}\n" +
+                  $"\t Date of Claim: \t{claim.DateOfClaim}\n" +
+                  $"\t IsValid: \t\t{claim.IsValid}\n" +
+                  $"..................................................................................");
+                    // Console.WriteLine($"\t {claim.ClaimID} \t\t {claim.Type} \t\t {claim.Description} \t\t {claim.Amount} \t\t {claim.DateOfAccident} \t\t {claim.DateOfClaim} \t\t {claim.IsValid}\n");
+                }
+                else
+                {
+                    Console.WriteLine("\n\t Can not find Claim");
+                }
             }
             else
             {
-                Console.WriteLine("\n\t Can not find Claim");
+                Console.WriteLine("\n\t Queue is empty ");
             }
         }
 
         private void DeleteClaim()
         {
             Console.Clear();
-            SeeIDandDate();
-            Console.Write("\n\t Enter claim ID# that you wish to delete\n" +
-                "\t ID#");
-            int itemDelete = Convert.ToInt32(Console.ReadLine());
-
-            Console.Write($"\n\t Are you sure you want to delete Claim #{itemDelete}? y/n ");
-            string input = Console.ReadLine();
-
-            switch (input.ToLower())
+            if (_repo.ClaimsCount() > 0)
             {
-                case "y":
-                case "yes":
-                    bool wasDeleted = _repo.DeleteClaimByID(itemDelete);
+                SeeIDandDate();
+                Console.Write("\n\t Enter claim ID# that you wish to delete\n" +
+                    "\t ID#");
+                int itemDelete = Convert.ToInt32(Console.ReadLine());
 
-                    if (wasDeleted)
-                    {
-                        Console.WriteLine("\n\tClaim was deleted");
-                    }
-                    else
-                    {
-                        Console.WriteLine("\n\tCould not delete Claim");
-                    }
-                    break;
-                case "n":
-                case "no":
-                    Console.WriteLine("\n\t Claim was not deleted");
-                    Console.ReadKey();
-                    break;
-                default:
-                    Console.WriteLine("\n\t Not a valid input.\n" +
-                        "\n\t Claim was not deleted");
-                    Console.ReadKey();
-                    DeleteClaim();
-                    break;
+                Console.Write($"\n\t Are you sure you want to delete Claim #{itemDelete}? y/n ");
+                string input = Console.ReadLine();
 
+                switch (input.ToLower())
+                {
+                    case "y":
+                    case "yes":
+                        bool wasDeleted = _repo.DeleteClaimByID(itemDelete);
+
+                        if (wasDeleted)
+                        {
+                            Console.WriteLine("\n\tClaim was deleted");
+                        }
+                        else
+                        {
+                            Console.WriteLine("\n\tCould not delete Claim");
+                        }
+                        break;
+                    case "n":
+                    case "no":
+                        Console.WriteLine("\n\t Claim was not deleted");
+                        Console.ReadKey();
+                        break;
+                    default:
+                        Console.WriteLine("\n\t Not a valid input.\n" +
+                            "\n\t Claim was not deleted");
+                        Console.ReadKey();
+                        DeleteClaim();
+                        break;
+
+                }
             }
+            else
+            {
+                Console.WriteLine("\n\t Queue is empty ");
+            }
+
         }
 
         private void SeedContent()
